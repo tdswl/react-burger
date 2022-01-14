75abcd584a91758d069c54c09ff3ea472e41e703
todayswl <todayswl@protonmail.com>
2022-01-14 17:52:03 +0300

    [sprint1] [step2] Modal to functional component

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -2,41 +2,36 @@ import ReactDOM from "react-dom";
 import React from "react";
 import styles from './modal.module.css'
 import ModalOverlay from "../modal-overlay/modal-overlay";
-import {Button, CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components'
 import PropTypes from "prop-types";
-import IngredientDetailParam from "../ingredient-details-param/ingredient-details-param";
 
 const modalRoot = document.getElementById("react-modals");
 
-class Modal extends React.Component {
-    // Устанавливаем слушатель события при монтировании
-    componentDidMount() {
-        document.addEventListener("keydown", this.props.onClose);
-    }
+const Modal = (props) => {
+    React.useEffect(()=>{
+        document.addEventListener("keydown", props.onClose);
 
-    // Сбрасываем слушатель события при удалении компонента из DOM
-    componentWillUnmount() {
-        document.removeEventListener("keydown", this.props.onClose, );
-    }
+        return () => {
+            document.removeEventListener("keydown", props.onClose );
+        }
+    }, [])
 
-    render() {
-        const {children, header, onClose} = this.props;
+    const {children, header, onClose} = this.props;
 
-        return ReactDOM.createPortal(
-            <ModalOverlay onClose={onClose}>
-                <div className={styles.modal}>
-                    <div className={styles.header}>
-                        {header && <p className="text text_type_main-large">
-                            {header}
-                        </p>}
-                        <CloseIcon type="primary" onClick={onClose} />
-                    </div>
-                    {children}
+    return ReactDOM.createPortal(
+        <ModalOverlay onClose={onClose}>
+            <div className={styles.modal}>
+                <div className={styles.header}>
+                    {header && <p className="text text_type_main-large">
+                        {header}
+                    </p>}
+                    <CloseIcon type="primary" onClick={onClose} />
                 </div>
-            </ModalOverlay>,
-            modalRoot
-        );
-    }
+                {children}
+            </div>
+        </ModalOverlay>,
+        modalRoot
+    );
 }
 
 Modal.propTypes = {
